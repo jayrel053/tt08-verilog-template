b3ee7acb579041e44ca3d50c3eb4f4e2a530a82a
jayrel053 <jrlong@uwaterloo.ca>
2025-05-19 14:44:28 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -15,13 +15,27 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+    reg [3:0] count_out;
+  
+  always @ (posedge clk)
+    if (~rst_n)
+      count_out <= 0;
+    else
+      count_out <= count_out+1;
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uo_out[0]=count_out[0];
+  assign uo_out[1]=count_out[1];
+  assign uo_out[2]=count_out[2];
+  assign uo_out[3]=count_out[3];
+  assign uo_out[4] = 0
+  assign uo_out[5] = 0
+  assign uo_out[6] = 0
+  assign uo_out[7] = 0
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, 1'b0};
 
 endmodule
