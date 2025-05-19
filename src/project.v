0d13c7ade61bd0fcc3bc7cd09453a3081c290d4d
jayrel053 <jrlong@uwaterloo.ca>
2025-05-19 13:43:11 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,9 +5,7 @@
 
 `default_nettype none
 
-module tt_um_full_adder (
-   // input  wire carry_in, 
-   // output wire carry_out,
+module tt_um_example (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -19,17 +17,9 @@ module tt_um_full_adder (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  
-  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  // assign uo_out  = !ui_in;  // Example: ou_out is the logical inverse of ui_in
-  // assign carry_in = 0;
-  // assign carry_out = 0;
-  // assign ui_in = 0;
-  // assign uio_in = 0;
-  assign uo_out = 0;
+  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
-  assign uo_out = ui_in + uio_in;
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
