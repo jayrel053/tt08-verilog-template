a9d20ec5232c5f8e88493344b8b3e62a1cc65752
jayrel053 <jrlong@uwaterloo.ca>
2025-10-01 16:50:02 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -23,7 +23,7 @@ async def test_project(dut):
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
+    dut._log.info("Test project behaviour")
 
     # Set the input values you want to test
     dut.ui_in.value = 20
