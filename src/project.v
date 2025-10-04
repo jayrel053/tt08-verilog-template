88675db1f149a63e2ae68531a2485ba2c56772e2
jayrel053 <jrlong@uwaterloo.ca>
2025-10-04 15:28:24 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -17,11 +17,19 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-    assign uo_out = ui_in[0] + uio_in[0];  // Example: ou_out is ui_in OR uio_in
+    always @(posedge clk or posedge rst_n)
+        if (rst_n)
+      assign uo_out <= 0;
+    else
+      assign uo_out <= uo_out + 1;
+    
+    //assign uo_out = ui_in[0] + uio_in[0];  // Example: ou_out is ui_in OR uio_in
+  assign ui_in = 0;
+  assign uio_in = 0;
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, 1'b0};
 
 endmodule
